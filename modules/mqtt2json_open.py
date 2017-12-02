cdb98f68c22f7caa67251dc6677c8c568dc22c3e
Tadeusz Magura-Witkowski <teeed@na1noc.pl>
2017-12-02 22:38:21 +0100

    Added module from mqtt2json

---
+++ modules/mqtt2json_open.py
@@ -0,0 +1,26 @@
+__version__ = 1.0
+__cacheable__ = True
+
+import urllib2
+import json
+from application import config
+
+CONFIG_KEY = 'module_mqtt2json_open'
+
+def update_document(data):
+	remote = urllib2.urlopen(config.get(CONFIG_KEY, 'url'), timeout=config.getint(CONFIG_KEY, 'timeout')).read()
+	remote = json.loads(remote)
+
+	if remote['zm/hs_active']['timestamp'] < time.time() - 120:
+		return
+
+	is_open = remote['zm/hs_active']['payload'] == 1
+	
+	if not data.get('sensors'):
+		data['sensors'] = {}
+
+		if not data.get('state'):
+			data['state'] = {}
+
+		data['state']['open'] = is_open
+		
